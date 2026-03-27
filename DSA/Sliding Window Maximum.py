e67822a7f6219b654e26c202e8435cf61c49a4e0
Yuvaraj <yuvicardy18@gmail.com>
2026-03-27 22:32:02 +0530

    Add files via upload

---
+++ DSA/Sliding Window Maximum.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+    
+        res = []
+        dq = deque()  
+        
+        for i in range(len(nums)):
+            
+            while dq and nums[dq[-1]] < nums[i]:
+                dq.pop()
+            
+            dq.append(i)
+  
+            if dq[0] == i - k:
+                dq.popleft()
+            
+            if i >= k - 1:
+                res.append(nums[dq[0]])
+                
+        return res
